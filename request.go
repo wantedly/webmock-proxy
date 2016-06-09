90889a1e0fcbb1224395c3c0df829df61983be7b
munisystem <munisystem@gmail.com>
2016-06-09 18:13:40 +0900

    Move function

--- request.go
+++ request.go
@@ -1,9 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 func getReqStruct(f file) request {
 	return parseReqStruct(convertJSONToStruct(readFile(f.Path)))
 }
 
 func parseReqStruct(con connection) request {
 	return con.Request
+}
+
+func readRequestBody(r *http.Request) string {
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		// TODO: Add error handling
+		fmt.Println("Oops...")
+	}
+	return string(body)
 }
